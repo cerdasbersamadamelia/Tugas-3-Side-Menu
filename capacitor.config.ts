14087e9827fbf6d43ff5610bf1d182ac111c41a9
unknown <pandaidancepat@gmail.com>
2022-11-25 10:11:09 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Tugas3App',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
